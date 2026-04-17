5fe3e344d99cb99980e54cd090c6a22b629ad8c9
NerdyElaine <167090657+NerdyElaine@users.noreply.github.com>
2026-04-17 17:18:54 +0700

    Changed CPU script to be more accurate

--- home/darwin/sketchybar.nix
+++ home/darwin/sketchybar.nix
@@ -68,8 +68,14 @@ let
   '';
 
   cpuPlugin = pkgs.writeShellScript "cpu.sh" ''
-    CPU=$(top -l 1 -n 0 | grep "CPU usage" | awk '{print $3}' | tr -d '%')
-    sketchybar --set "$NAME" label="$CPU% |"
+    CORE_COUNT=$(sysctl -n machdep.cpu.thread_count)
+    CPU_INFO=$(ps -eo pcpu,user)
+    CPU_SYS=$(echo "$CPU_INFO" | grep -v $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
+    CPU_USER=$(echo "$CPU_INFO" | grep $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
+
+    CPU_PERCENT="$(echo "$CPU_SYS $CPU_USER" | awk '{printf "%.0f\n", ($1 + $2)*100}')"
+
+    sketchybar --set $NAME label="$CPU_PERCENT% |"
   '';
 
   # Memory plugin script
